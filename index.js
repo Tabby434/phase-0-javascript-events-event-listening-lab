0c59bb9a40ba79b652d379deb242a87885b449f2
Tabitha Wangechi <twangechi742@gmail.com>
2025-04-01 12:01:51 +0300

    my first commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  // Function to add the event listener
+  function addingEventListener() {
+    const input = document.getElementById("button");
+    input.addEventListener("click", function () {
+      alert("I was clicked!");
+    });
+  }
+
+  // Call the function to activate the event listener
+  addingEventListener();
 }
